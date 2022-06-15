f4dbea52b902812508e9fb5912096d05304318cf
Mahmud Hyader <81094755+Mahmud291@users.noreply.github.com>
2022-06-15 22:07:46 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
-from django.urls import  path
-from django.urls.conf import include
-from course import views
-urlpatterns = [
-    path('cou',views.cou),
-    path('show',views.show),
-    path('edit/<int:code>, views.edit),
-    path('upadte/<int:code>, views.update),
-    path('delete/<int:code>, views.destory),
 
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
